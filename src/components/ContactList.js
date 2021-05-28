e95118d28a1e7079adc23ecebcbab6dc8d843597
Wahid Shaikh <shaikh23wahid@gmail.com>
2021-05-28 10:33:53 +0530

    Initial commit

---
+++ src/components/ContactList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ContactCard from "./ContactCard";
+
+const ContactList = (props) => {
+  const deleteContactHandler = (id) => {
+    props.getContactId(id);
+  };
+
+  const renderContactList = props.contacts.map((contact) => {
+    return (
+      <ContactCard
+        contact={contact}
+        clickHandler={deleteContactHandler}
+        key={contact.id}
+      ></ContactCard>
+    );
+  });
+
+  return <div className="ui called list">{renderContactList}</div>;
+};
+
+export default ContactList;
